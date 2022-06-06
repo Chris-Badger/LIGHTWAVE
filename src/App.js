05d46b978f84e77d9d6ebc322d5e12fc6e915472
chris-badger <contactbadger@gmail.com>
2022-06-05 22:35:43 -0700

    Refactor with Grid Area Template

--- src/App.js
+++ src/App.js
@@ -5,24 +5,24 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        GITHUB TEST 3
+        GITHUB TEST 4
       </header>
-      <div class="slidecontainer" >
+      <div class="grid-container">
+  <div class="item1"><div class="slidecontainer" >
           <input type="range" min="0" max="255" defaultValue="0" class="slider" id="redRange" />
-          <p>Red: <span id="redValue"></span></p>
-        </div>
-        <div class="slidecontainer">
-          <input type="range" min="0" max="255" defaultValue="0" class="slider" id="greenRange" />
-          <p>Green: <span id="greenValue"></span></p>
-        </div>
-        <div class="slidecontainer">
-          <input type="range" min="0" max="255" defaultValue="0" class="slider" id="blueRange" />
-          <p>Blue: <span id="blueValue"></span></p>
-        </div>
-        <div>
-        <RGB_SLIDERS/>
-        </div>
-        
+        </div></div>
+  <div class="item2"><div class="slidecontainer">
+          <input type="range" min="0" max="255" defaultValue="0" class="slider" id="greenRange" /> 
+        </div></div>
+  <div class="item3"><div class="slidecontainer">
+          <input type="range" min="0" max="255" defaultValue="0" class="slider" id="blueRange" />  
+        </div></div>  
+  <div class="item4"> <p>Red: <span id="redValue"> </span> </p> </div>
+  <div class="item5"><p>Green: <span id="greenValue"></span></p></div>
+  <div class="item6"><p>Blue: <span id="blueValue"></span></p></div>
+  
+</div>  
+<div> <RGB_SLIDERS/> </div>
     </div>
   );
 }
