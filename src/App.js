ce8bba050a1c8f918b846048e8397eee4e6030c7
chris-badger <contactbadger@gmail.com>
2022-06-14 21:47:57 -0700

    LFO sliders on all channels

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import RGB_SLIDERS from './components/RGB_Sliders';
+import SYNTH_OUTPUT from './components/SYNTH_OUTPUT';
 import './App.css';
 
 function App() {
@@ -9,27 +9,40 @@ function App() {
       </header>
 
       <div class="grid-container">
-  <div class="item1"><div class="slidecontainer" >
+        <div class="item1"><div class="slidecontainer" >
           <input type="range" min="0" max="255" defaultValue="0" class="slider" id="redRange" />
         </div></div>
-  <div class="item2"><div class="slidecontainer">
-          <input type="range" min="0" max="255" defaultValue="0" class="slider" id="greenRange" /> 
+        <div class="item9"><div class="slidecontainer" >
+          <input type="range" min="0" max="1000" defaultValue="0" class="slider" id="frequency_range2" />
         </div></div>
-  <div class="item3"><div class="slidecontainer">
-          <input type="range" min="0" max="255" defaultValue="0" class="slider" id="blueRange" />  
-        </div></div>  
-
+        <div class="item2"><div class="slidecontainer">
+          <input type="range" min="0" max="255" defaultValue="0" class="slider" id="greenRange" />
+        </div></div>
+        <div class="item12"><div class="slidecontainer">
+          <input type="range" min="0" max="1000" defaultValue="0" class="slider" id="frequency_range3" />
+        </div></div>
+        <div class="item3"><div class="slidecontainer">
+          <input type="range" min="0" max="255" defaultValue="0" class="slider" id="blueRange" />
+        </div></div>
+        <div class="item13"><div class="slidecontainer">
+          <input type="range" min="0" max="1000" defaultValue="0" class="slider" id="frequency_range4" />
+        </div></div>
         <div class="item7"><div class="slidecontainer">
-          <input type="range" min="0" max="100" defaultValue="0" class="slider" id="frequency_range" />  
-        </div></div> 
+          <input type="range" min="0" max="1000" defaultValue="0" class="slider" id="frequency_range" />
+        </div></div>
 
-  <div class="item4"> <p>Red: <span id="redValue"> </span> </p> </div>
-  <div class="item5"><p>Green: <span id="greenValue"></span></p></div>
-  <div class="item6"><p>Blue: <span id="blueValue"></span></p></div>
-  <div class="item8"><p>Frequency: <span id="lfo_value"></span></p></div>
-  
-</div>  
-<div> <RGB_SLIDERS/> </div>
+
+        <div class="item4"> <p>Red: <span id="redValue"> </span> </p> </div>
+        <div class="item10"> <p>Red Mod: <span id="lfo2_value"> </span> </p> </div>
+        <div class="item5"><p>Green: <span id="greenValue"></span></p></div>
+        <div class="item11"><p>Green Mod: <span id="lfo3_value"></span></p></div>
+        <div class="item6"><p>Blue: <span id="blueValue"></span></p></div>
+        <div class="item14"><p>Blue Mod: <span id="lfo4_value"></span></p></div>
+        <div class="item8"><p>Alpha Mod: <span id="lfo_value"></span></p></div>
+
+
+      </div>
+      <div> <SYNTH_OUTPUT /> </div>
     </div>
   );
 }
---
+++ src/components/SYNTH_OUTPUT.js
@@ -0,0 +1,168 @@
+import React from "react";
+import { ReactP5Wrapper } from "react-p5-wrapper";
+
+function RGB_sketch(p5) {
+
+
+  let r = 0;
+  let g = 0;
+  let b = 0;
+  let alpha = 255;
+  // Declare Alfa Mod Variables
+  let lfo = 0;
+  let lfo_output = 0;
+  let lfo_frequency = 0;
+  // Declare red Mod Variables
+  let lfo2 = 0;
+  let lfo2_output = 0;
+  let lfo2_frequency = 0;
+  // Declare green Mod Variables
+  let lfo3 = 0;
+  let lfo3_output = 0;
+  let lfo3_frequency = 0;
+  // Declare blue Mod Variables
+  let lfo4 = 0;
+  let lfo4_output = 0;
+  let lfo4_frequency = 0;
+
+
+  p5.setup = () => {
+    p5.createCanvas(p5.windowWidth, p5.windowHeight);
+  };
+
+  p5.draw = () => {
+    p5.background(0);
+    // build the alpha Mod LFO
+    lfo = lfo + lfo_frequency;
+    lfo_output = p5.sin(lfo);
+
+    // build the red Mod LFO2
+    lfo2 = lfo2 + lfo2_frequency;
+    lfo2_output = p5.sin(lfo2);
+
+    // build the green Mod LFO3
+    lfo3 = lfo3 + lfo3_frequency;
+    lfo3_output = p5.sin(lfo3);
+
+    // build the green Mod LFO3
+    lfo4 = lfo4 + lfo4_frequency;
+    lfo4_output = p5.sin(lfo4);
+
+    // display a rectangle of color
+    p5.fill(r, g, b, alpha);
+    p5.rect(0, 0, p5.width, p5.height);
+
+
+    // conect RGB sliders 
+    var redSlider = document.getElementById("redRange");
+    var redOutput = document.getElementById("redValue");
+    redOutput.innerHTML = redSlider.value; // Display the default slider value
+
+    // Update the current slider value (each time you drag the slider handle)
+    redSlider.oninput = function () {
+      redOutput.innerHTML = this.value;
+    };
+
+    var greenSlider = document.getElementById("greenRange");
+    var greenOutput = document.getElementById("greenValue");
+    greenOutput.innerHTML = greenSlider.value; // Display the default slider value
+
+    // Update the current slider value (each time you drag the slider handle)
+    greenSlider.oninput = function () {
+      greenOutput.innerHTML = this.value;
+    };
+
+    var blueSlider = document.getElementById("blueRange");
+    var blueOutput = document.getElementById("blueValue");
+    blueOutput.innerHTML = blueSlider.value; // Display the default slider value
+
+    // Update the current slider value (each time you drag the slider handle)
+    blueSlider.oninput = function () {
+      blueOutput.innerHTML = this.value;
+    };
+
+    // conect the alpha Mod lfo frequency slider 
+    var frequency_slider = document.getElementById("frequency_range");
+    var frequency_slider_output = document.getElementById("lfo_value");
+    frequency_slider_output.innerHTML = frequency_slider.value; // Display the default slider value
+
+    // conect the red Mod lfo2 frequency slider 
+    var frequency_slider2 = document.getElementById("frequency_range2");
+    var frequency_slider2_output = document.getElementById("lfo2_value");
+    frequency_slider2_output.innerHTML = frequency_slider2.value; // Display the default slider value
+
+    // conect the green Mod lfo3 frequency slider 
+    var frequency_slider3 = document.getElementById("frequency_range3");
+    var frequency_slider3_output = document.getElementById("lfo3_value");
+    frequency_slider3_output.innerHTML = frequency_slider3.value; // Display the default slider value
+
+    // conect the blue Mod lfo4 frequency slider 
+    var frequency_slider4 = document.getElementById("frequency_range4");
+    var frequency_slider4_output = document.getElementById("lfo4_value");
+    frequency_slider4_output.innerHTML = frequency_slider4.value; // Display the default slider value
+
+
+    // Update the alpha Mod slider value (each time you drag the slider handle)
+    frequency_slider.oninput = function () {
+      frequency_slider_output.innerHTML = this.value;
+    };
+
+    // Update the red Mod slider value (each time you drag the slider handle)
+    frequency_slider2.oninput = function () {
+      frequency_slider2_output.innerHTML = this.value;
+    };
+
+    // Update the green Mod slider value (each time you drag the slider handle)
+    frequency_slider3.oninput = function () {
+      frequency_slider3_output.innerHTML = this.value;
+    };
+
+    // Update the blue Mod slider value (each time you drag the slider handle)
+    frequency_slider4.oninput = function () {
+      frequency_slider4_output.innerHTML = this.value;
+    };
+
+    r = redSlider.value;
+    g = greenSlider.value;
+    b = blueSlider.value;
+    lfo_frequency = frequency_slider.value / 1000;
+    lfo2_frequency = frequency_slider2.value / 1000;
+    lfo3_frequency = frequency_slider3.value / 1000;
+    lfo4_frequency = frequency_slider4.value / 1000;
+
+    // modulate alpha with the lfo
+    alpha = p5.map(lfo_output, -1, 1, 0, 255);
+    // set default alpha value
+    if (frequency_slider.value == 0) {
+      alpha = 255;
+    }
+
+    // modulate red with  lfo2
+    r = p5.map(lfo2_output, -1, 1, 0, redSlider.value);
+    // set default alpha value
+    if (frequency_slider2.value == 0) {
+      r = redSlider.value;
+    }
+
+    // modulate green with  lfo3
+    g = p5.map(lfo3_output, -1, 1, 0, greenSlider.value);
+    // set default alpha value
+    if (frequency_slider3.value == 0) {
+      g = greenSlider.value;
+    }
+
+    // modulate blue with  lfo4
+    b = p5.map(lfo4_output, -1, 1, 0, blueSlider.value);
+    // set default alpha value
+    if (frequency_slider4.value == 0) {
+      b = blueSlider.value;
+    }
+  };
+
+}
+
+export function SYNTH_OUTPUT() {
+  return <ReactP5Wrapper sketch={RGB_sketch} />;
+}
+
+export default SYNTH_OUTPUT;
